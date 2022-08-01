4e52281f8317221febdc877ab87738148765636a
tg024 <temuri.grdzelidze.1@btu.edu.ge>
2022-08-01 18:36:51 +0400

    Added users data

---
+++ src/main/java/DataObject/AddNewUserData.java
@@ -0,0 +1,13 @@
+package DataObject;
+
+public interface AddNewUserData {
+    String
+            validFullName = "John",
+            invalidFormatName = "123",
+            validEmailAddress = "dsajmcdsvc@valid.hj",
+            invalidEmail = "text",
+            validRole = "Commercialista",
+            emptyRole = "",
+            emptyFullName = "",
+            emptyEmail = "";
+}
